0ce8e49b57b8e3c79c331cd045dd942a200eadc2
spanishprisoner <jacek.mowinski@gmail.com>
2021-10-02 19:27:43 +0200

    Something showing

--- src/SotCookingOverlay/Program.cs
+++ src/SotCookingOverlay/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace SotCookingOverlay
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			WndProc delegWndProc = myWndProc;
+
+			WNDCLASSEX windowClassEx = new WNDCLASSEX();
+			windowClassEx.cbSize = Marshal.SizeOf(typeof(WNDCLASSEX));
+			windowClassEx.style = (int)(WinAPI.CS_HREDRAW | WinAPI.CS_VREDRAW | WinAPI.CS_DBLCLKS); //Doubleclicks are active
+			windowClassEx.hbrBackground = (IntPtr)WinAPI.COLOR_BACKGROUND + 1; //Black background, +1 is necessary
+			windowClassEx.cbClsExtra = 0;
+			windowClassEx.cbWndExtra = 0;
+			windowClassEx.hInstance = Process.GetCurrentProcess().Handle;
+			windowClassEx.hIcon = IntPtr.Zero;
+			windowClassEx.hCursor = WinAPI.LoadCursor(IntPtr.Zero, (int)WinAPI.IDC_CROSS);// Crosshair cursor;
+			windowClassEx.lpszMenuName = null;
+			windowClassEx.lpszClassName = "myClass";
+			windowClassEx.lpfnWndProc = Marshal.GetFunctionPointerForDelegate(delegWndProc);
+			windowClassEx.hIconSm = IntPtr.Zero;
+			ushort registerClassEx = WinAPI.RegisterClassEx(ref windowClassEx);
+
+			IntPtr hwnd = WinAPI.CreateWindowExModified(0, registerClassEx, "Hello Win32", WinAPI.WS_OVERLAPPEDWINDOW | WinAPI.WS_VISIBLE, 0, 0, 300, 400, IntPtr.Zero, IntPtr.Zero, windowClassEx.hInstance, IntPtr.Zero);
+			WinAPI.ShowWindow(hwnd, 1);
+			WinAPI.UpdateWindow(hwnd);
+
+			await Task.Delay(3000);
+		}
+
+		private static IntPtr myWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+		{
+			switch (msg)
+			{
+				case WinAPI.WM_PAINT:
+					break;
+
+				default:
+					break;
+			}
+			return WinAPI.DefWindowProc(hWnd, msg, wParam, lParam);
 		}
 	}
 }
---
+++ src/SotCookingOverlay/WinAPI.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SotCookingOverlay
+{
+	delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+	public struct WNDCLASSEX
+	{
+		[MarshalAs(UnmanagedType.U4)]
+		public int cbSize;
+		[MarshalAs(UnmanagedType.U4)]
+		public int style;
+		public IntPtr lpfnWndProc;
+		public int cbClsExtra;
+		public int cbWndExtra;
+		public IntPtr hInstance;
+		public IntPtr hIcon;
+		public IntPtr hCursor;
+		public IntPtr hbrBackground;
+		public string lpszMenuName;
+		public string lpszClassName;
+		public IntPtr hIconSm;
+	}
+
+	public class WinAPI
+	{
+		public const UInt32 WS_OVERLAPPEDWINDOW = 0xcf0000;
+		public const UInt32 WS_VISIBLE = 0x10000000;
+		public const UInt32 CS_USEDEFAULT = 0x80000000;
+		public const UInt32 CS_DBLCLKS = 8;
+		public const UInt32 CS_VREDRAW = 1;
+		public const UInt32 CS_HREDRAW = 2;
+		public const UInt32 COLOR_WINDOW = 5;
+		public const UInt32 COLOR_BACKGROUND = 1;
+		public const UInt32 IDC_CROSS = 32515;
+		public const UInt32 WM_DESTROY = 2;
+		public const UInt32 WM_PAINT = 0x0f;
+		public const UInt32 WM_LBUTTONUP = 0x0202;
+		public const UInt32 WM_LBUTTONDBLCLK = 0x0203;
+
+		[DllImport("user32.dll", SetLastError = true, EntryPoint = "RegisterClassEx")]
+		public static extern UInt16 RegisterClassEx([In] ref WNDCLASSEX lpWndClass);
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+
+		[DllImport("user32.dll", SetLastError = true, EntryPoint = "CreateWindowEx")]
+		public static extern IntPtr CreateWindowEx(
+			int dwExStyle,
+			string lpClassName,
+			string lpWindowName,
+			UInt32 dwStyle,
+			int x,
+			int y,
+			int nWidth,
+			int nHeight,
+			IntPtr hWndParent,
+			IntPtr hMenu,
+			IntPtr hInstance,
+			IntPtr lpParam);
+
+		[DllImport("user32.dll", SetLastError = true, EntryPoint = "CreateWindowEx")]
+		public static extern IntPtr CreateWindowExModified(
+			int dwExStyle,
+			UInt16 regResult,
+			//string lpClassName,
+			string lpWindowName,
+			UInt32 dwStyle,
+			int x,
+			int y,
+			int nWidth,
+			int nHeight,
+			IntPtr hWndParent,
+			IntPtr hMenu,
+			IntPtr hInstance,
+			IntPtr lpParam);
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		[DllImport("user32.dll")]
+		public static extern bool UpdateWindow(IntPtr hWnd);
+	}
+}
